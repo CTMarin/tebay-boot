600281ed1408a02d59ac817955d819bfd18ae309
carlo <carlomaco11@gmail.com>
2022-05-23 10:11:06 +0200

    First Commit

---
+++ src/test/java/es/uma/tebayboot/TebayBootApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.tebayboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TebayBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
