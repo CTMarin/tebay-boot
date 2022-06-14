c6e8d7b02d873479c83885bee925fc590abfddc9
carmengo01 <carmela050701@gmail.com>
2022-06-14 18:03:41 +0200

    new: UsuarioRepository and Fav

---
+++ src/main/java/es/uma/tebayboot/dao/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package es.uma.tebayboot.dao;
+
+import es.uma.tebayboot.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity,Integer> {
+
+}
---
+++ src/main/java/es/uma/tebayboot/dto/Fav.java
@@ -0,0 +1,23 @@
+package es.uma.tebayboot.dto;
+
+public class Fav {
+
+    private Integer idSubasta;
+    private boolean fav;
+
+    public Integer getIdSubasta() {
+        return idSubasta;
+    }
+
+    public void setIdSubasta(Integer idSubasta) {
+        this.idSubasta = idSubasta;
+    }
+
+    public boolean isFav() {
+        return fav;
+    }
+
+    public void setFav(boolean fav) {
+        this.fav = fav;
+    }
+}
